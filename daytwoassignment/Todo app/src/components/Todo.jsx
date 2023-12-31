f9ba69dbc9da4aa94834a4d67a2e31c374415dfc
Mamta sharma <your_email@example.com>
2023-12-31 10:08:00 -0800

    todo app with add and delete functionality

---
+++ daytwoassignment/Todo app/src/components/Todo.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+
+function Todo() {
+  const [item, setItem] = useState("");
+  const [todolist, setTodoList] = useState([]);
+
+  const handleAddTodo = () => {
+    setTodoList([...todolist, { item: item }]);
+    setItem(''); 
+  };
+
+  const handleDeleteTodo = (index) => {
+    
+    setTodoList([...todolist.slice(0, index), ...todolist.slice(index + 1)]);
+  };
+
+  return (
+    <Container >
+
+      <div>
+        
+        <input
+          type="text"
+          placeholder='Enter todo item'
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>ADD</button>
+      </div>
+      <div>
+        <Row>
+         
+            <ul>
+              {todolist.map((items, index) => (
+                <li key={index}>{items.item}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+       
+          </li>
+              ))}
+                </ul>
+               
+              
+          
+           
+        </Row>
+      </div>
+    </Container>
+  );
+
+              }
+export default Todo
+              
